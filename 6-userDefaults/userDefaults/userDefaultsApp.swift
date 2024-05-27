99dcdf35ab04b37be8f63b15cc29a2afbcb09446
Emre Cihan <yunusemrecihan@gmail.com>
2024-05-27 19:26:55 +0300

    Add 6-userDefaults

---
+++ 6-userDefaults/userDefaults/userDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  userDefaultsApp.swift
+//  userDefaults
+//
+//  Created by Yunus emre cihan on 21.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct userDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
